149a90b895eb121c9234f0c60af9dee8eb582736
Arnav-Soney <148421763+Arnav-Soney@users.noreply.github.com>
2025-06-19 22:55:27 +0530

    minor fixes

--- backend/constants.js
+++ backend/constants.js
@@ -1,1 +1,6 @@
 export const DB_NAME = "aeodb"
+export const VALIDATION_ERROR= "400"
+export const UNAUTHORIZED= "401"
+export const FORBIDDEN= "403"
+export const NOT_FOUND= "404"
+export const SERVER_ERROR= "500"
