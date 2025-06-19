d621117fc5aeaa643114584db509e32f5b9f968e
Arnav-Soney <148421763+Arnav-Soney@users.noreply.github.com>
2025-06-19 20:23:26 +0530

    changes in models

--- backend/models/application.models.js
+++ backend/models/application.models.js
@@ -1,36 +1,35 @@
 // models/Application.js
-import mongoose from 'mongoose'
+import mongoose, { Schema } from 'mongoose'
 //{id, type, status, formProgress, annexures}
-const applicationSchema = new mongoose.Schema(
-  {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
-    applicationType: {
-      type: String,
-      enum: ['T1', 'T2', 'T3', 'LO', 'MSME'],
-      required: true,
-    },
-    status: {
-      type: String,
-      enum: ['draft', 'submitted', 'in_review', 'approved', 'rejected'],
-      default: 'draft',
-    },
-    formProgress: {
-      type: Map,
-      of: Boolean,
-      default: {},
-    },
-    annexures: {
-      type: Map,
-      of: mongoose.Schema.Types.ObjectId, // Links to Annexure documents
-      ref: 'Annexure',
-      default: {},
-    },
-  },
-  { timestamps: true }
+const applicationSchema = new Schema({
+	userId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		required: true,
+	},
+	applicationType: {
+		type: String,
+		enum: ['T1', 'T2', 'T3', 'LO', 'MSME'],
+		required: true,
+	},
+	status: {
+		type: String,
+		enum: ['draft', 'submitted', 'in_review', 'approved', 'rejected'],
+		default: 'draft',
+	},
+	formProgress: {
+		type: Map,
+		of: Boolean,
+		default: {},
+	},
+	annexures: {
+		type: Map,
+		of: mongoose.Schema.Types.ObjectId, // Links to Annexure documents
+		ref: 'Annexure',
+		default: {},
+	},
+},
+	{ timestamps: true }
 );
 
 module.exports = mongoose.model('Application', applicationSchema);
