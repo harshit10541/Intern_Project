8c4966a6ef9970c7a41ebe3e379ac63ba04845cd
Harshit <harshit10541@gmail.com>
2025-06-21 11:54:56 +0530

    application routes working

--- backend/routes/applications.routes.js
+++ backend/routes/applications.routes.js
@@ -5,9 +5,9 @@ import {
     updateApplication
 } from '../controllers/applicationController.js'
 
-const router = Router()
+const router = Router();
 
 router.route('/').post(createApplication)
 router.route('/:id').get(getApplication).put(updateApplication)
 
-export default router
+export default router;
