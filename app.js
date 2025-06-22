8d2f665ffa2a6eecacb34b8514385cef94878eed
Arnav-Soney <148421763+Arnav-Soney@users.noreply.github.com>
2025-06-22 20:15:55 +0530

    Restored working files after reinitializing git

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import { errorHandler } from "./middleware/errorHandler.js"
+
+const app = express()
+
+//Middleware
+// app.use(cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true
+// }))
+app.use(express.json());
+app.use(errorHandler);  // middleware
+
+
+//Routes import and all defined in one place and main route in this to make it more modular
+// routes declaration (we use this to use the middleware)
+
+import userRoutes from './routes/user.routes.js'
+import annexuresRoutes from './routes/annexures.routes.js'
+import applicationsRoutes from './routes/applications.routes.js'
+
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/annexures', annexuresRoutes);
+app.use('/api/v1/applications', applicationsRoutes);
+
+// app.get('/', (req, res) =>{
+//     res.json("Get all");
+// });
+
+
+export { app }
